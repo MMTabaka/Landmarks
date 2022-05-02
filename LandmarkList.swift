0eb543b6a7ce99eb53295bf12820b6ff9e61b5c6
MMTabaka <miloszmtabaka@gmail.com>
2022-05-02 23:35:12 +0100

    Section 10 - Create the List of Landmarks

---
+++ LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Milosz Tabaka on 02/05/2022.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
